325819ec1f2ff88d39279ef080c9a82162033e57
Harut Qanaqaryan <hqanaqaryan@gmail.com>
2021-03-09 20:38:48 +0300

    add catalog images

--- script.js
+++ script.js
@@ -5,32 +5,27 @@ function plusSlides(n) {
     showSlides(slideIndex += n);
 }
 
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
 
 function showSlides(n) {
-    let slides = document.getElementsByClassName("mySlides");
-    if (n > slides.length) { 
-        slideIndex = 1 
+    let slides = document.getElementsByClassName("mySlides"); // querySelector
+    if (n > slides.length) {
+        slideIndex = 1
     }
-    if (n < 1) { 
-        slideIndex = slides.length 
+    if (n < 1) {
+        slideIndex = slides.length
     }
     for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += "active";
 }
 
 
-function openbox(id){
-      let display = document.getElementById(id).style.display;
-        if(display=='none'){
-           document.getElementById(id).style.display='block';
-        }else{
-           document.getElementById(id).style.display='none';
-        }
+function openbox(id) {
+    let openBurgerMenu = document.getElementById(id).style.display;
+    if (openBurgerMenu == 'none') {
+        document.getElementById(id).style.display = 'block';
+    } else {
+        document.getElementById(id).style.display = 'none';
     }
-    
+}
