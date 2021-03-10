6ab641667385c57a93e1aea6793d7a4b4932f53b
Harut Qanaqaryan <hqanaqaryan@gmail.com>
2021-03-10 20:19:25 +0300

    update

--- script.js
+++ script.js
@@ -1,61 +1,55 @@
 let slideIndex = 1;
 showSlides(slideIndex);
 
-function plusSlides(n) {
-    showSlides(slideIndex += n);
+function nextSlide() {
+    showSlides(slideIndex += 1);
 }
 
+function prevSlide() {
+    showSlides(slideIndex -= 1);  
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
 
 function showSlides(n) {
-    let slides = document.getElementsByClassName("mySlides"); // querySelector
+    let i;
+    let slides = document.getElementsByClassName("item");
+    
     if (n > slides.length) {
-        slideIndex = 1
+      slideIndex = 1
     }
     if (n < 1) {
         slideIndex = slides.length
     }
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex - 1].style.display = "block";
+  
+    for (let slide of slides) {
+        slide.style.display = "none";
+    }   
+    slides[slideIndex - 1].style.display = "block"; 
 }
 
 
 //  ===== Open Burger Menu =====
 
 let openBurgerMenu = document.querySelector('.burger-icon')
-let closeCatalog = document.querySelector(".close-burger-icon")
 let burgerMenu = document.querySelector('.box')
 
 openBurgerMenu.addEventListener("click", () => {
     if (burgerMenu.style.display == 'none') {
         burgerMenu.style.display = 'block';
-    } 
-    if(closeCatalog.style.display == "none"){
-        closeCatalog.style.display = "block"
+    } else {
+        burgerMenu.style.display = "none"
     }
-    if(openBurgerMenu.style.display == "block"){
-        openBurgerMenu.style.display = "none"
-    }
-}) 
-// ===== Close Burger Menu ====
+    }) 
 
-closeCatalog.addEventListener("click", () => {
-    if(burgerMenu.style.display == 'block'){
-        burgerMenu.style.display = 'none';
-    }
-    if(openBurgerMenu.style.display == "none"){
-        openBurgerMenu.style.display = "block";
-    }
-    if(closeCatalog.style.display == "block"){
-        closeCatalog.style.display = "none"
-    }
-})
 
 // ===== Open Catalog of Burger-menu ====
 
 let openCatalog = document.querySelector(".burger-menu-catalog");
 let burgerMenuCatalog = document.querySelector(".burger-menu-dropdown-content")
+let iconCloseCatalog = document.querySelector(".close-burger-menu-dropdown-content")
 
 openCatalog.addEventListener("click", () => {
     if (burgerMenuCatalog.style.display == "none") {
@@ -64,5 +58,14 @@ openCatalog.addEventListener("click", () => {
         burgerMenuCatalog.style.display = "none";
     }
 })
+
+// ===== Close Catalog of Burger-menu ====
+
+iconCloseCatalog.addEventListener("click", () => {
+    if (burgerMenuCatalog.style.display == "block") {
+        burgerMenuCatalog.style.display = "none"
+    }
+})
+
 
 
