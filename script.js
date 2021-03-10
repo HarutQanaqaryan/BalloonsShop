966e60aede94de9312e732e8b2da92ff99745ffc
Harut Qanaqaryan <hqanaqaryan@gmail.com>
2021-03-10 17:31:57 +0300

    update

--- script.js
+++ script.js
@@ -21,11 +21,48 @@ function showSlides(n) {
 }
 
 
-function openbox(id) {
-    let openBurgerMenu = document.getElementById(id).style.display;
-    if (openBurgerMenu == 'none') {
-        document.getElementById(id).style.display = 'block';
-    } else {
-        document.getElementById(id).style.display = 'none';
+//  ===== Open Burger Menu =====
+
+let openBurgerMenu = document.querySelector('.burger-icon')
+let closeCatalog = document.querySelector(".close-burger-icon")
+let burgerMenu = document.querySelector('.box')
+
+openBurgerMenu.addEventListener("click", () => {
+    if (burgerMenu.style.display == 'none') {
+        burgerMenu.style.display = 'block';
+    } 
+    if(closeCatalog.style.display == "none"){
+        closeCatalog.style.display = "block"
     }
-}
+    if(openBurgerMenu.style.display == "block"){
+        openBurgerMenu.style.display = "none"
+    }
+}) 
+// ===== Close Burger Menu ====
+
+closeCatalog.addEventListener("click", () => {
+    if(burgerMenu.style.display == 'block'){
+        burgerMenu.style.display = 'none';
+    }
+    if(openBurgerMenu.style.display == "none"){
+        openBurgerMenu.style.display = "block";
+    }
+    if(closeCatalog.style.display == "block"){
+        closeCatalog.style.display = "none"
+    }
+})
+
+// ===== Open Catalog of Burger-menu ====
+
+let openCatalog = document.querySelector(".burger-menu-catalog");
+let burgerMenuCatalog = document.querySelector(".burger-menu-dropdown-content")
+
+openCatalog.addEventListener("click", () => {
+    if (burgerMenuCatalog.style.display == "none") {
+        burgerMenuCatalog.style.display = "block"
+    }else {
+        burgerMenuCatalog.style.display = "none";
+    }
+})
+
+
