6ff0a582b4f573f2325fb7f97b9bdc1d3acf55c5
Harut Qanaqaryan <hqanaqaryan@gmail.com>
2021-03-11 16:28:38 +0300

    add classList.toggle() for burger menu

--- script.js
+++ script.js
@@ -3,11 +3,9 @@ let currentSlide = 0;
 let slideInterval = setInterval(nextSlide,3000);
 
 function nextSlide(){
-  slides[currentSlide].className = 'slide';
   currentSlide = (currentSlide+1)%slides.length;
   slides[currentSlide].className = 'slide showing';
 }
-
 
 
 //  ===== Open Burger Menu =====
@@ -16,11 +14,12 @@ let openBurgerMenu = document.querySelector('.burger-icon')
 let burgerMenu = document.querySelector('.box')
 
 openBurgerMenu.addEventListener("click", () => {
-    if (burgerMenu.style.display == 'none') {
-        burgerMenu.style.display = 'block';
-    } else {
-        burgerMenu.style.display = "none"
-    }
+    // if (burgerMenu.style.display == 'none') { 
+    //     burgerMenu.style.display = 'block';
+    // } else {
+    //     burgerMenu.style.display = "none"
+    // }
+       burgerMenu.classList.toggle("box");
     }) 
 
 
@@ -31,19 +30,21 @@ let burgerMenuCatalog = document.querySelector(".burger-menu-dropdown-content")
 let iconCloseCatalog = document.querySelector(".close-burger-menu-dropdown-content")
 
 openCatalog.addEventListener("click", () => {
-    if (burgerMenuCatalog.style.display == "none") {
-        burgerMenuCatalog.style.display = "block"
-    }else {
-        burgerMenuCatalog.style.display = "none";
-    }
+    // if (burgerMenuCatalog.style.display == "none") {
+    //     burgerMenuCatalog.style.display = "block"
+    // }else {
+    //     burgerMenuCatalog.style.display = "none";
+    // }
+    burgerMenuCatalog.classList.toggle('burger-menu-dropdown-content')
 })
 
 // ===== Close Catalog of Burger-menu ====
 
 iconCloseCatalog.addEventListener("click", () => {
-    if (burgerMenuCatalog.style.display == "block") {
-        burgerMenuCatalog.style.display = "none"
-    }
+    // if (burgerMenuCatalog.style.display == "block") {
+    //     burgerMenuCatalog.style.display = "none"
+    // }
+    burgerMenuCatalog.classList.toggle('burger-menu-dropdown-content')
 })
 
 
